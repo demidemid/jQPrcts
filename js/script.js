813a7bff2c02da49dd350cba3f8dedf720ca7fda
Demid Miroshnichenko <iamdemid@gmail.com>
2017-06-06 16:27:24 +0300

    animate.css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 console.log($);
 
-$('.open-me').click(function() {
-  $('.hide-layout, .popup').fadeIn(300);  // плавно показываем окно/фон
+$(".open-me").click(function() {
+  $(".popup").removeClass("hinge");
+  $(".hide-layout, .popup").fadeIn(300);  // плавно показываем окно/фон
 })
-$('.close-me, .hide-layout').click(function() {
-  $('.hide-layout, .popup').fadeOut(300); // плавно скрываем окно/фон
+$(".close-me, .hide-layout").click(function() {
+  $(".popup").addClass("hinge");
+  $(".hide-layout").fadeOut(300); // плавно скрываем окно/фон
 })
