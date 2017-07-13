d3b5b171ec9653f4342b165811dc5d6c56a4bab4
Demid Miroshnichenko <iamdemid@gmail.com>
2017-07-13 15:47:43 +0300

    spoiler + tel input

---
+++ js/skillbox.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".spoiler-link").click(function(){
+
+    $("div.spoiler-content").not($(this).parent().children("div.spoiler-content")).slideUp("normal");
+    $(this).parent().children("div.spoiler-content").slideDown("normal");
+    return false;
+
+  });
+});
+
+ $(".tel").mask("+7 (999) 999-9999",{placeholder:"+7 (___) ___-____"});
